9d14e519699161293026486ccec1ac0f7276d1cc
Loan Cardoso <jow13_122@yahoo.com>
2020-09-22 18:56:09 -0300

    Initial commit

---
+++ public/scripts/index.js
@@ -0,0 +1,9 @@
+const buttonSearch = document.querySelector("#page-home .content main a");
+const modal = document.querySelector("#modal");
+const close = document.querySelector("#modal .content .header a");
+buttonSearch.addEventListener("click", () => {
+    modal.classList.remove("hide");
+})
+close.addEventListener("click", () => {
+    modal.classList.add("hide");
+})
